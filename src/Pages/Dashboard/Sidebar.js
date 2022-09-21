bbeaf2590595461485427854ab506d0924451908
unknown <simeonolawale69@gmail.com>
2022-09-21 11:12:50 +0530

    dashboard

--- src/Pages/Dashboard/Sidebar.js
+++ src/Pages/Dashboard/Sidebar.js
@@ -6,8 +6,36 @@ import profile from "../../Component/Images/profile (2).png";
 import Order from "../../Component/Images/order.png";
 import cart from "../../Component/Images/cart.png";
 import "./sass/dash.css";
+import avatar from "../../Component/Images/avatar.svg"
+import pasta from '../../Component/Images/pasta.svg';
+import burger from '../../Component/Images/burger.svg';
+import balls from '../../Component/Images/balls.svg';
+import spagg from '../../Component/Images/spagg.svg';
+import bread from '../../Component/Images/bread.svg';
+import china from '../../Component/Images/china.svg';
 
 const Sidebar = () => {
+  let userDetails = JSON.parse(sessionStorage.getItem("user",))
+  const foods = [
+    {id:1, foodname:"Stir Fry Pasta", image: pasta, content:"The in-house pasta and chicken by chef Moose", price:"N 1,000.00"},
+    {id:2, foodname:"Stir Fry Pasta", image: burger, content:"The in-house pasta and chicken by chef Moose", price:"N 1,000.00"},
+    {id:3, foodname:"Stir Fry Pasta", image: balls, content:"The in-house pasta and chicken by chef Moose", price:"N 1,000.00"},
+   // {id:4, foodname:"Stir Fry Pasta", image: spagg, content:"The in-house pasta and chicken by chef Moose", price:"N 1,000.00"},
+    //{id:5, foodname:"Stir Fry Pasta", image: bread, content:"The in-house pasta and chicken by chef Moose", price:"N 1,000.00"},
+    //{id:6, foodname:"Stir Fry Pasta", image: china, content:"The in-house pasta and chicken by chef Moose", price:"N 1,000.00"},
+    //{id:7, foodname:"Stir Fry Pasta", image: china, content:"The in-house pasta and chicken by chef Moose", price:"N 1,000.00"},
+    
+
+  ]
+  const secondfoods = [
+    {id:1, foodname:"Stir Fry Pasta", image: spagg, content:"The in-house pasta and chicken by chef Moose", price:"N 1,000.00"},
+    {id:2, foodname:"Stir Fry Pasta", image: bread, content:"The in-house pasta and chicken by chef Moose", price:"N 1,000.00"},
+    {id:3, foodname:"Stir Fry Pasta", image: china, content:"The in-house pasta and chicken by chef Moose", price:"N 1,000.00"},
+  ]
+
+  
+
+  
   return (
     <div className="dashboard">
 
@@ -30,17 +58,55 @@ const Sidebar = () => {
           <div className="sideicon">
             <img src={Order} alt="icon" />
             <p>Orders</p>
-            <button>6</button>
+            <button className="btn1">6</button>
           </div>
           <div className="sideicon">
             <img src={cart} alt="icon" />
             <p>Your Cart</p>
-            <button>6</button>
+            <button className="btn2">6</button>
           </div>
         </div>
       </div>
       <div className="mainPage">
-        <p>mainpage</p>
+        <div className="header">
+          <div>
+            <h2>Good morning, {userDetails.name} !</h2>
+            <p>What delicious meal are you craving today?</p>
+          </div>
+          <div><img src={avatar}   alt='avatar'/></div>
+        </div>
+        <div className="contents">
+          {
+            foods && foods.map((food)=>
+            <div className="cards" key={food.id}>
+               <img src={food.image} className='foodImage' alt='pasta'/>
+               <h2>{food.foodname}</h2>
+               <p>The in-house pasta and <br/> chicken by chef Moose</p>
+               <div className="addprice">
+                <div className="price">{food.price}</div>
+                <div><button>Add to cart</button></div>
+               </div>
+
+            </div>
+            )
+          }
+        </div>
+        <div className="contents">
+          {
+            secondfoods && secondfoods.map((secondfood)=>
+            <div className="cards" key={secondfood.id}>
+               <img src={secondfood.image} className='foodImage' alt='pasta'/>
+               <h2>{secondfood.foodname}</h2>
+               <p>The in-house pasta and <br/> chicken by chef Moose</p>
+               <div className="addprice">
+                <div className="price">{secondfood.price}</div>
+                <div><button>Add to cart</button></div>
+               </div>
+
+            </div>
+            )
+          }
+        </div>
       </div>
     </div>
   );
